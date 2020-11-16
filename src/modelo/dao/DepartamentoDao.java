ec50defe08c68d71b71f4fb72d37506b22b38511
fabiogrod <fabiogrimello@gmail.com>
2020-11-16 15:40:42 -0300

    Aula 244 - Interfaces DepartamentoDao e VendedorDao

---
+++ src/modelo/dao/DepartamentoDao.java
@@ -0,0 +1,18 @@
+package modelo.dao;
+
+import java.util.List;
+
+import modelo.entidades.Departamento;
+
+public interface DepartamentoDao {
+	
+	void insere(Departamento departamento);
+	
+	void atualiza(Departamento departamento);
+	
+	void deletaId(Integer id);
+	
+	Departamento pesquisarId(Integer id);
+	
+	List<Departamento> pesquisar();
+}
---
+++ src/modelo/dao/VendedorDao.java
@@ -0,0 +1,18 @@
+package modelo.dao;
+
+import java.util.List;
+
+import modelo.entidades.Vendedor;
+
+public interface VendedorDao {
+
+void insere(Vendedor Vendedor);
+	
+	void atualiza(Vendedor Vendedor);
+	
+	void deletaId(Integer id);
+	
+	Vendedor pesquisarId(Integer id);
+	
+	List<Vendedor> pesquisar();
+}
